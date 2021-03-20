5a687583c537c41d0bd6a7dd2eeeed4453514a3b
The-MinGo <3047673038@qq.com>
2021-03-20 19:37:03 +0800

    Create hello_flask.py

---
+++ hello_flask.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+"""
+To run this app in PowerShell:
+1. $env:FLASK_APP "hello_flask.py"
+2. flask run --host=0.0.0.0
+"""
+@app.route('/test/<username>')
+def hello_flask():
+    return "Hello Flask!"
